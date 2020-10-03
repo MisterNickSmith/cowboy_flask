64284173a0c4c69d465aa97f45dce3e3f6be2e97
nick <nick@cruiserhouse.com>
2020-10-03 12:19:18 -0700

    updated

--- projects/app/app/views.py
+++ projects/app/app/views.py
@@ -7,30 +7,33 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home') 
 
-bar ={
-         "first" : "projects" ,
-         "second" : "resume" ,
-         "third" : "contact",
-       }
+
 
 def home(): 
+#  bar = { 'first' : 'projects' ,   'second' : 'resume' ,'third' : 'contact'}
   return render_template("base.html", title = "Nick Smith")
-
-@app.projects('/projects')
+@app.route('/projects')
 
 def projects(): 
   return render_template("projects.html", title = "Projects") 
-
+@app.route('/resume')
+def resume(): 
+  return render_template("resume.html", title= "Resume")
+@app.route('/contact')
 def contact(): 
   return render_template("contact.html", title = "Contact")
 
 
+def styling():
+ url_for( 'static', filename="styling.css")
 
 
 
 
 
-  if __name__ == "__main__":
+
+
+if __name__ == "__main__":
     app.run(debug=true)
 
 
