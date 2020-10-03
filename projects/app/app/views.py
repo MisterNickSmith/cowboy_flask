ca999b6e3b075fe8d8b552e8ad7e92ff468f2e9e
nick <nick@cruiserhouse.com>
2020-10-03 08:11:05 -0700

    updated

--- projects/app/app/views.py
+++ projects/app/app/views.py
@@ -1,10 +1,37 @@
-from app import app
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+#why imported from app and not flask? 
 
 @app.route('/')
-#@app.route('/home') 
+@app.route('/home') 
 
-def index(): 
-  return "Hello World!" 
+bar ={
+         "first" : "projects" ,
+         "second" : "resume" ,
+         "third" : "contact",
+       }
+
+def home(): 
+  return render_template("base.html", title = "Nick Smith")
+
+@app.projects('/projects')
+
+def projects(): 
+  return render_template("projects.html", title = "Projects") 
+
+def contact(): 
+  return render_template("contact.html", title = "Contact")
+
+
+
+
+
+
+
+  if __name__ == "__main__":
+    app.run(debug=true)
 
 
 
